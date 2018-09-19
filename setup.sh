a3759c2a62b22ecaccb40ffe76bfa32554627411
NasserHumood <nooh43@live.com>
2018-09-19 00:27:31 -0600

    1- divide into 2 steps 2- silent first part 3- add steps to first part

--- setup.sh
+++ setup.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 # Ruby On Rails initializer - RORi
-# Version : V 0.4.5
+# Version : V 0.5.0
 # Author  : Nasser Alhumood
 # .-.    . . .-.-.
 # |.|.-.-|-.-|-`-..-,.-.. .
 # `-``-`-'-' ' `-'`'-'   `
 
 # Some Unnecessary Variables, but they're here anyway
-version=V0.4.5
+version=V0.5.0
 oss="CentOs7, RHEL7"
+dir=/etc/profile.d/rvm_secure_path.sh
 
 # Clear
 clear
@@ -21,29 +22,45 @@ echo +=================================
 echo
 echo
 
-# Making sure you wanna continue
-read -p "would you like to continue ? [y/N] "
-echo
-if [[ ! $REPLY =~ ^[Yy]$ ]]
+# making sure user loginout and in again
+if [ ! -d "$dir" ];
 then
-  echo No problem, goodbye!
+  # Making sure you wanna continue
+  read -p "would you like to continue ? [y/N] "
+  echo
+  if [[ ! $REPLY =~ ^[Yy]$ ]]
+  then
+    echo No problem, goodbye!
+    exit 0
+  fi
+
+  # Step 1 : Updating the systems
+  echo "SYSTEM UPDATE               [in progress]"
+  sudo yum -y update &>/dev/null
+  echo "SYSTEM UPDATE               [   +done   ]"
+  # Step 2 : installing epel
+  echo "EPEL INSTALLATION           [in progress]"
+  sudo yum -y install yum install epel-release &>/dev/null
+  sudo yum -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm &>/dev/null
+  sudo yum -y update &>/dev/null
+  echo "EPEL INSTALLATION           [   +done   ]"
+  # Step 3 : Prepare the system and install dependencies
+  echo "INSTALL DEPENDENCIES        [in progress]"
+  sudo yum install -y curl gpg gcc gcc-c++ make &>/dev/null
+  echo "INSTALL DEPENDENCIES        [   +done   ]"
+  # Step 4 : install rvm
+  echo "INSTALL RVM                 [in progress]"
+  sudo gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 &>/dev/null
+  curl -sSL https://get.rvm.io | sudo bash -s stable &>/dev/null
+  sudo usermod -a -G rvm `whoami` &>/dev/null
+  if sudo grep -q secure_path /etc/sudoers; then sudo sh -c "echo export rvmsudo_secure_path=1 >> /etc/profile.d/rvm_secure_path.sh" && echo Environment variable installed; fi &>/dev/null
+  echo "INSTALL RVM                 [   +done   ]"
+  # Step 5 : ask to logout and login again to continue
+  echo "Awesome we are half way through it, now you have to logout from the terminal then login again"
   exit 0
 fi
 
-# Step 1 : Updating the systems
-sudo yum -y update
-# Step 2 : installing epel
-sudo yum -y install yum install epel-release
-sudo yum -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
-sudo yum -y update
-# Step 3 : Prepare the system and install dependencies
-sudo yum install -y curl gpg gcc gcc-c++ make
-# Step 4 : install rvm
-sudo gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
-curl -sSL https://get.rvm.io | sudo bash -s stable
-sudo usermod -a -G rvm `whoami`
-if sudo grep -q secure_path /etc/sudoers; then sudo sh -c "echo export rvmsudo_secure_path=1 >> /etc/profile.d/rvm_secure_path.sh" && echo Environment variable installed; fi
-# Step 5 : ask to logout and login again to continue
+echo "Cool you are in step two now!"
 
 # Step 6 : installing ruby
 rvm install ruby
